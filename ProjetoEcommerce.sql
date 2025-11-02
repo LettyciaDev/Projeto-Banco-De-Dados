02eae1324cff196a95feadbb73234e33e7c26fa2
joao0cb <joao.00000853774@unicap.br>
2025-11-02 20:47:15 -0300

    Novos usuarios com privilegios e view de vendas detalhadas

--- ProjetoEcommerce.sql
+++ ProjetoEcommerce.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS projeto;
 
 USE projeto;
 
-CREATE TABLE clientes(
+CREATE TABLE IF NOT EXISTS cliente(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     idade TINYINT NOT NULL,
@@ -10,39 +10,42 @@ CREATE TABLE clientes(
     data_n DATE NOT NULL
 );
 
-CREATE TABLE vendedores(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS vendedor(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     causa_s VARCHAR(50) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
-    nota_m DECIMAL(10, 2) NOT NULL
+    cargo VARCHAR(20) CHECK(cargo = "vendedor" OR cargo = "gerente" OR cargo = "CEO"),
+    nota_media DECIMAL(10, 2) NOT NULL,
+    valor_vendido DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    salario DECIMAL(10, 2) NOT NULL DEFAULT 0.00
 );
 
-CREATE TABLE transportadora(
+CREATE TABLE IF NOT EXISTS transportadora(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
     transporte VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE clientes_especiais(
+CREATE TABLE IF NOT EXISTS cliente_especial(
 	id INT PRIMARY KEY,
     cashback DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (id) REFERENCES clientes(id)
+    FOREIGN KEY (id) REFERENCES cliente(id)
 );
 
-CREATE TABLE produto (
+CREATE TABLE IF NOT EXISTS produto(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     descr VARCHAR(50) NOT NULL,
     qtd_e INT,
     valor DECIMAL(10, 2) NOT NULL,
     obs VARCHAR(35) NOT NULL,
-	vendedor_id INT NOT NULL, 
-    FOREIGN KEY (vendedor_id) REFERENCES vendedores(id)
+	id_vendedor INT NOT NULL, 
+    FOREIGN KEY (id_vendedor) REFERENCES vendedor(id)
 );
 
-CREATE TABLE venda(
+CREATE TABLE IF NOT EXISTS venda(
     id INT PRIMARY KEY AUTO_INCREMENT,
     data_venda DATE NOT NULL,
     hora TIME NOT NULL,
@@ -52,7 +55,41 @@ CREATE TABLE venda(
     FOREIGN KEY (id_cliente) REFERENCES cliente(id),
     FOREIGN KEY (id_produto) REFERENCES produto(id),
     FOREIGN KEY (id_transp) REFERENCES transportadora(id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS venda_produto(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_venda INT NOT NULL,
+    id_produto INT NOT NULL,
+    qtd INT NOT NULL DEFAULT 1,
+    valor DECIMAL(10, 2) NOT NULL,
+    obs VARCHAR(35) NOT NULL,
+    FOREIGN KEY (id_venda) REFERENCES venda(id),
+    FOREIGN KEY (id_produto) REFERENCES produto(id)
+);
+
+CREATE USER IF NOT EXISTS 'admin@localhost' IDENTIFIED BY 'administrador20251102';
+CREATE USER IF NOT EXISTS 'gerenteloja@localhost' IDENTIFIED BY 'gerentecargo123';
+CREATE USER IF NOT EXISTS 'funcionario@localhost' IDENTIFIED BY 'funcionario543';
+
+GRANT ALL PRIVILEGES ON projeto.* TO 'admin@localhost' WITH GRANT OPTION;
+GRANT SELECT, DELETE, UPDATE ON projeto.* TO 'gerenteloja@localhost';
+GRANT SELECT, INSERT ON projeto.venda TO 'funcionario@localhost';
+
+FLUSH PRIVILEGES;
+
+CREATE OR REPLACE VIEW vendas_detalhadas AS
+SELECT venda.id AS id_venda,
+cliente.nome AS nome_cliente,
+produto.nome AS nome_produto,
+vendedor.nome AS vendedor_nome,
+venda.data_venda, venda.hora, venda_produto.qtd, venda_produto.valor,
+(venda_produto.qtd * venda_produto.valor) AS total_venda
+FROM venda
+JOIN venda_produto ON venda_produto.id_venda = venda.id
+JOIN produto ON venda_produto.id_produto = produto.id
+JOIN vendedor ON produto.id_vendedor = vendedor.id
+JOIN cliente ON venda.id_cliente = cliente.id;
 
 DELIMITER //
 CREATE FUNCTION Calcula_idade (cliente_id INT)
@@ -63,17 +100,11 @@ BEGIN
 
     SELECT data_n INTO data_nascimento
     FROM clientes
-    WHERE id = cliente_id
+    WHERE id = cliente_id;
 
-    SET idade = TIMESTAMDIFF(YEAR, data_nascimento, CURDATE());
+    SET idade = TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE());
 
     RETURN idade;
-
 END //
-
-DELIMITER;
-
-
 DELIMITER //
-
 
